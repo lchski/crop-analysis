0800847804af5884ea22631c362540932eafb798
Lucas Cherkewski <lucas@ecustom.ca>
2018-02-23 17:49:18 -0500

    add topic codes

--- analysis.R
+++ analysis.R
@@ -5,8 +5,10 @@ source("./functions.R")
 
 # Setup
 outputDb <- dbConnect(drv = RSQLite::SQLite(), dbname = "output.sqlite")
+topicCodeStore <- read.csv("codes.csv", header = TRUE)
 
-# Base data frames
+# Base data objects
+topicCodes <- topicCodeStore$code
 sampledIssues <- dbGetQuery(outputDb, "SELECT * FROM issues WHERE in_sample = 1")
 
 # Topic code data frames
